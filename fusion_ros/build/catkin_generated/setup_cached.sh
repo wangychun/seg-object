c71598c4f83c4d39a139859859b3bfa458ac048e
sarah <wangyuchun0208@163.com>
2019-10-24 16:42:41 +0800

    will fusion image

---
+++ fusion_ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sarah/program/mine/seg_obj_fast_ros/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-10.0/lib64"
+export PATH="/opt/ros/kinetic/bin:/usr/bin:/usr/local/cuda-10.0/bin:/home/sarah/bin:/home/sarah/.local/bin:/java/jdk1.8.0_121/bin:/java/jdk1.8.0_121/jre/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/java/jdk1.8.0_121/bin:/snap/bin"
+export PWD="/home/sarah/program/mine/seg_obj_fast_ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sarah/program/mine/seg_obj_fast_ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/sarah/program/mine/seg_obj_fast_ros/src:$ROS_PACKAGE_PATH"
---
+++ fusion_ros/build/seg/CMakeFiles/seg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seg.dir/src/main.cpp.o"
+  "CMakeFiles/seg.dir/src/gridmap.cpp.o"
+  "CMakeFiles/seg.dir/src/pic_handle.cpp.o"
+  "/home/sarah/program/mine/seg_obj_fast_ros/devel/lib/seg/seg.pdb"
+  "/home/sarah/program/mine/seg_obj_fast_ros/devel/lib/seg/seg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
